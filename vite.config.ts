e9cbdf24efc3876ad55f9fb6173229a766b53e0e
Munyia <ndidiranjo@gmail.com>
2025-06-10 19:47:08 +0100

    update

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/WordS/', // 👈 This line is important for GitHub Pages
   plugins: [react()],
+  base: '/', // Important: tells Vite to resolve assets from root
 })
